baaf5deed1e2c451b09b6dc0837c406909a37003
PrashanthKR7 <krakrprashu@gmail.com>
2018-07-22 00:21:37 +0530

    minor changes

--- src/auth/initAuth.js
+++ src/auth/initAuth.js
@@ -1,6 +1,6 @@
 import AuthService from "./authService";
 const auth = new AuthService(
-  process.env.HYPHEN_DEMO_AUTHs_CLIENT_ID,
+  process.env.HYPHEN_DEMO_AUTH_CLIENT_ID,
   process.env.HYPHEN_DEMO_AUTH_DOMAIN_ADDRESS
 );
 export default auth;
