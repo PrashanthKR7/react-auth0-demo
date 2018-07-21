9040ef392c501c39d7311c20d7193df1e461ff0f
PrashanthKR7 <krakrprashu@gmail.com>
2018-07-22 00:54:25 +0530

    minor changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,69 @@
-import React, { Component } from 'react';
-import logo from '../../assets/img/logo.svg'
-import '../css/style.css'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import logo from "../../assets/img/logo.svg";
+import "../css/style.css";
+import { Link } from "react-router-dom";
 
 //material-ui components
-import { TextField } from 'material-ui/';
+import { TextField } from "material-ui/";
 
 //theme related material-ui
-import { MuiThemeProvider,
-         getMuiTheme,
-         darkBaseTheme } from 'material-ui/styles'
+import {
+  MuiThemeProvider,
+  getMuiTheme,
+  darkBaseTheme
+} from "material-ui/styles";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoginToggle: true,
+      isLoggedIn: auth.loggedIn()
+    };
+  }
+
+  _loginSignUpToggle = () => {
+    this.setState({ isLoginToggle: !this.state.isLoginToggle });
+  };
+
+  _refresh = () => {
+    this.forceUpdate();
+  };
+
+  _updateStatus = () => {
+    this.setState({ isLoggedIn: auth.loggedIn() });
+  };
+
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
         <div className="App">
           <div className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React with Material UI Boilerplate</h2>
+            <h2>Welcome to a vanilla React with react-router 4</h2>
+            <h2>with Auth0 Authentication</h2>
           </div>
 
           <div className="border color-blue">
-              <Link to='/PageOne'>Go to Page One</Link>
-              <br/>
-              <TextField
-                hintText="Type something here"
-                floatingLabelText="This is a text field from App component"
-                fullWidth={true}
-              />
+            <Link to="/PageOne">Go to Page One</Link>
           </div>
 
-           {/* Render children here*/}
-           {this.props.children} 
+          <button onClick={this._loginSignUpToggle}>
+            Click to {this.state.isLoginToggle ? "Sign Up" : "Login"}
+          </button>
+
+          {this.state.isLoginToggle ? (
+            <Login
+              _refresh={this._refresh}
+              _updateStatus={this._updateStatus}
+            />
+          ) : (
+            <SignUp />
+          )}
+
+          {/* Render children here*/}
+          {this.props.children}
         </div>
       </MuiThemeProvider>
     );
