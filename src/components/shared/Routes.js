aae94f9353c3901e0342125d66444e77c3419efb
PrashanthKR7 <krakrprashu@gmail.com>
2018-07-21 23:34:05 +0530

    bug fixes

--- src/components/shared/Routes.js
+++ src/components/shared/Routes.js
@@ -6,7 +6,7 @@ import NotFound from "./NotFound";
 import App from "./../App";
 import PageOne from "./../PageOne";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 const PrivateRoute = ({component, ...rest}) => (
   <Route {...rest} render={props => (
@@ -22,7 +22,7 @@ const Routes = props => {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={App} />
-        <Route path="/PageOne" component={PageOne} />
+        <PrivateRoute path="/PageOne" component={PageOne} />
         <Route path="/NotFound" component={NotFound} />  
         <Route component={NotFound} />
       </Switch>
